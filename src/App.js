ad79c68abdb52de6696f6e1f35e078565069e182
boneswvb@gmail.com <boneswvb@gmail.com>
2019-06-18 14:42:39 +0200

    Final update - phase one

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
-function App() {
+const particleOptions = {
+particles: {
+     number: {
+      value: 50,
+      density: {
+        enable: true,
+        value_area: 400
+      }
+    }
+  }
+ }
+
+class App extends Component {
+	render() {
   return (
     <div className="App">
+    	 <Particles className='particles'
+        params={ particleOptions } 
+        />
       <Navigation />
       <Logo />
       <Rank />
       <ImageLinkForm />
       {/* <FaceRecognition />*/}
     </div>
-  );
+  	);
+	}
 }
 
 export default App;
