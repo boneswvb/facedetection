f2b9ce93b00dbb015d5483a7d9738baeb8309479
boneswvb@gmail.com <boneswvb@gmail.com>
2019-06-18 07:12:11 +0200

    new module

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+  return(
+    <div className='white f3'>
+    	<div>
+    		{'Wim, your current rank is...'}
+    	</div>
+    	<div className='white f1'>
+    		{'#1'}
+    	</div>
+    </div>
+  );
+}
+
+export default Navigation;
